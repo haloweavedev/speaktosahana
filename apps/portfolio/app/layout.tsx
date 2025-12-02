c81c9465fb52bd0957b2edf1d642769755f9fe35
justDenzil <vikash.denzil@gmail.com>
2025-12-02 18:52:08 +0530

    first commit

--- apps/portfolio/app/layout.tsx
+++ apps/portfolio/app/layout.tsx
@@ -13,6 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <script defer src="https://cloud.umami.is/script.js" data-website-id="1f516028-f9aa-44a0-baec-95f00bc07366"></script>
       <body className="min-h-screen bg-background text-foreground antialiased">
         {children}
       </body>
